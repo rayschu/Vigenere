34984c1398f0d6ea40520c10b33db51a204f701b
Rayschu Wang <rayschu@gmail.com>
2016-02-14 10:10:18 +0800

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -16,7 +16,7 @@ def encrypt(plain, key):
     return cipher
 
 def decrypt(cipher, key):
-	cipher = cipher.replace(" ", "").upper()
+    cipher = cipher.replace(" ", "").upper()
     key = key.upper()
     key_index = 0
     plain = ''
@@ -30,36 +30,36 @@ def decrypt(cipher, key):
 
 
 def main():
-	decision = ""
-	while decision is not "1" and decision is not "2":
-		print "Would you like to encrypt or decrypt a message?"
-		print "1 - Encrypt message"
-		print "2 - Decrypt Message"
-		print "\nDecision:"
-		decision = raw_input()
+    decision = ""
+    while decision is not "1" and decision is not "2":
+	print "Would you like to encrypt or decrypt a message?"
+	print "1 - Encrypt message"
+	print "2 - Decrypt Message"
+	print "\nDecision:"
+	decision = raw_input()
 	
-	if decision == "1":
-		print "\nEncrypt Message:"
-		print "Enter the message you would like to encrypt:"
-		plain = raw_input()
-		print "Enter the key:"
-		key = raw_input()
-		ciphertext = encrypt(plain, key)
-		print("The Ciphertext is: ")
-		print(ciphertext)
+    if decision == "1":
+	print "\nEncrypt Message:"
+	print "Enter the message you would like to encrypt:"
+	plain = raw_input()
+	print "Enter the key:"
+	key = raw_input()
+	ciphertext = encrypt(plain, key)
+	print("The Ciphertext is: ")
+	print(ciphertext)
 
-	elif decision == "2":
-		print "\nDecrypt Message:"
-		print "Enter the message you would like to decrypt:"
-		cipher = raw_input()
-		print "Enter the key:"
-		key = raw_input()
-		plaintext = decrypt(cipher, key)
-		print("The Plaintext is: ")
-		print(plaintext)
+    elif decision == "2":
+	print "\nDecrypt Message:"
+	print "Enter the message you would like to decrypt:"
+	cipher = raw_input()
+	print "Enter the key:"
+	key = raw_input()
+	plaintext = decrypt(cipher, key)
+	print("The Plaintext is: ")
+	print(plaintext)
 
-	else:
-		print "Invalid Entry"
+    else:
+	print "Invalid Entry"
 
 if __name__ == "__main__":
 	main()
